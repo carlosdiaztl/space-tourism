dc78263920c0e7b5247c5df44d35d98b16590838
carlos diaz <108467585+carlosdiaztl@users.noreply.github.com>
2022-10-24 23:40:20 -0500

    letra home

--- src/components/elhome/Hhome.jsx
+++ src/components/elhome/Hhome.jsx
@@ -9,8 +9,8 @@ const Hhome = () => {
     </body>
     <div className='home'>
     <div><h4> So , you want to travel to</h4>
-    <h1>Space</h1>
-    <p> loreafdcadda daaaaaaaaaaaaaaa  adaaaaaaaaaaaaa addddddddddddddd addddddddddddd </p>
+    <h1>SPACE</h1>
+    <p> Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience! </p>
     </div>
     <aside className='home_aside'><span><button>EXPLORE</button>  </span></aside>
     </div>
